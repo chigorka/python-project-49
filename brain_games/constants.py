22474cb4cccee8bf8e68b99c023f2c3c08c26ac4
chigorka <egorpavlbelg@gmail.com>
2024-07-28 03:17:56 +0300

    add constants.py

---
+++ brain_games/constants.py
@@ -0,0 +1,7 @@
+NUMBER_OF_QUESTIONS = 3
+PROGRESSION_LENGTH = 10
+TASK_CALC = 'What is the result of the expression?'
+TASK_EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
+TASK_GCD = 'Find the greatest common divisor of given numbers.'
+TASK_PRIME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+TASK_PROGRESSION = 'What number is missing in the progression?'
